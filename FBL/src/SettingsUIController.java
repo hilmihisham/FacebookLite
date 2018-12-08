ef299d1156d598536acae7a3ab2c8d94e2e52ccd
Iyad Shaheen <ivs1990@yahoo.com>
2018-12-07 16:50:33 -0800

    Setting UI and ProfileUI

---
+++ FBL/src/SettingsUIController.java
@@ -0,0 +1,18 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+public class SettingsUIController {
+
+    @FXML
+    TextField UpdateAgeField;
+
+    @FXML
+    CheckBox HideFriendsCBox;
+
+    @FXML
+    CheckBox HideAgeCBox;
+
+    @FXML
+    CheckBox HidePostsCBox;
+}
